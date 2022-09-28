0e644206ab1dfb4c766a2f5b2617de6f1cfd37e1
DarrelA <darrelaiscoding@gmail.com>
2022-09-28 20:49:43 +0800

    10-photo-blog

---
+++ 10-photo-blog/04-upload-pictures/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("10-photo-blog/04-upload-pictures/templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	// process form submission
+	if req.Method == http.MethodPost {
+		mf, fh, err := req.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+		// create sha for file name
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+		// create new file
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "10-photo-blog", "04-upload-pictures", "public", "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		// copy
+		mf.Seek(0, 0)
+		/*
+			- The Seek() function which is a part of the Seeker interface in this case is being used to rewind the file back to its beginning position.
+			- Another way of writing this could have been
+
+			`mf.Seek(0, io.SeekStart)``
+			`io.SeekStart = 0``
+
+			- The offset parameter is relative to the first byte.
+			- So what is happening is we are telling the file to be start at the offset 0 which is the first bye.
+			- This ensures that we are getting all of the file and not part of it.
+		*/
+
+		io.Copy(nf, mf)
+		// add filename to this user's cookie
+		c = appendValue(w, c, fname)
+	}
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", xs)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	return c
+}
+
+// takes in a file name now also
+func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
+	s := c.Value
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
+}
