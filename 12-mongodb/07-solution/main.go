3e1af7c395d39309a6fa4335ba5e2fc0c691b674
DarrelA <darrelaiscoding@gmail.com>
2022-09-29 13:13:37 +0800

    12-mongodb

---
+++ 12-mongodb/07-solution/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/DarrelASandbox/go-web-development/12-mongodb/07_solution/controllers"
+	"github.com/DarrelASandbox/go-web-development/12-mongodb/07_solution/models"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+	// Get a UserController instance
+	uc := controllers.NewUserController(getSession())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:8080", r)
+}
+
+func getSession() map[string]models.User {
+	return make(map[string]models.User)
+}
---
+++ 12-mongodb/go.mod
@@ -0,0 +1,10 @@
+module github.com/DarrelASandbox/go-web-development/12-mongodb
+
+go 1.19
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/satori/go.uuid v1.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
+)
